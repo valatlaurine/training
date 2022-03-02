9ed5c8d3cbee856459474ebbeb41d58850e2a89e
laurine.valat <laurine.valat@neoxam.com>
2022-03-02 14:37:34 +0100

    feat: vue training

---
+++ vueproject/neoxam/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/index.html",
+            './src/App.vue', 
+            './src/components/KPI.vue',
+            './src/components/NewComponent.vue',
+            './src/components/PerfChart.vue',
+            './src/components/SideNav.vue',
+            './src/components/TopNav.vue',
+          ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
